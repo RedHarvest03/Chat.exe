1e24a42e809954828c1a76942bd58a6bbe73160b
stepguest <stepguest@STHQ0127-10>
2023-03-28 11:23:46 +0400

    Add project files.

---
+++ AzeriChat/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace SimpleChat;
+
+public partial class MainWindow : Window
+{
+    private List<string> messages = new List<string>();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnSend_Click(object sender, RoutedEventArgs e)
+    {
+        string message = txtMessage.Text;
+        messages.Add(message);
+        lstChat.Items.Add(message);
+        txtMessage.Text = "";
+    }
+}
+
